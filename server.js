0e82b050d9ff241f6cdd6ad4cbda5e947e03f653
Baranatalii <baranatalii@BARANs-MacBook-Air.local>
2026-01-13 11:25:30 -0500

    up to dlete all done

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const logger = require("morgan");
+const methodOverride = require("method-override");
 const db = require("./db/connection.js");
+const Fruit = require("./models/fruit.js");
 
 const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
 
 app.use(logger("dev"));
 
@@ -10,6 +15,45 @@ app.use(logger("dev"));
 app.get("/", (req, res) => {
   // res.send("You are on the landing page.")
   res.render("index.ejs");
+});
+
+app.get("/fruits/new", (req, res) => {
+  res.render("fruits/new.ejs");
+});
+
+app.post("/fruits", async (req, res) => {
+  if (req.body.isReadyToEat === "on") {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
+  await Fruit.create(req.body);
+
+  //console.log(req.body);
+  res.redirect("/fruits");
+});
+
+app.get("/fruits", async (req, res) => {
+  const allFruits = await Fruit.find({});
+
+  res.render("fruits/index.ejs", {
+    fruits: allFruits,
+  });
+});
+
+app.get("/fruits/:fruitId", async (req, res) => {
+  // get the fruit from the db by its id
+  const fruit = await Fruit.findById(req.params.fruitId);
+
+  res.render("fruits/show.ejs", {
+    fruit: fruit,
+  });
+});
+
+app.delete("/fruits/:fruitId", async (req, res) => {
+  await Fruit.findByIdAndDelete(req.params.fruitId);
+
+  res.redirect("/fruits");
 });
 
 db.on("connected", () => {
